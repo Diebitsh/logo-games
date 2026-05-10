17add26e3a1f8b855c3575a2298f631a940ab5c9
Jiraya <177346249+intjiraya@users.noreply.github.com>
2026-05-11 00:22:46 +0200

    Auto-generated commit

---
+++ chunk-G5FZXZUP.js
@@ -0,0 +1,1 @@
+function o(n){let t=n.length,e;for(;t>0;)e=Math.floor(Math.random()*t),t--,[n[t],n[e]]=[n[e],n[t]];return n}function h(n){return n[Math.floor(Math.random()*n.length)]}export{o as a,h as b};
